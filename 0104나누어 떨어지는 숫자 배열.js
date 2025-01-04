44e85046b297731b19e94429343750e4e15f193a
lyra <mk.lyra.j@gmail.com>
2025-01-04 21:11:16 +0900

    fix:코드 재확인 후 코멘트

--- 0104나누어 떨어지는 숫자 배열.js
+++ 0104나누어 떨어지는 숫자 배열.js
@@ -3,10 +3,12 @@ function solution(arr, divisor) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % divisor == 0) {
       answer.push(arr[i]);
-    } else if (answer.length == 0) {
-      answer.push(-1);
     }
   }
+  if (answer.length == 0) {
+    answer.push(-1);
+  }
+
   return answer.sort((a, b) => a - b);
 }
 
