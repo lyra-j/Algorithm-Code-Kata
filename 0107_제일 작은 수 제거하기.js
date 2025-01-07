8fcb818269a986d7b340f3996e17359527c57a3e
lyra <mk.lyra.j@gmail.com>
2025-01-07 10:52:38 +0900

    fix:주석 및 코드 수정

--- 0107_제일 작은 수 제거하기.js
+++ 0107_제일 작은 수 제거하기.js
@@ -1,10 +1,11 @@
-function solution(arr) {
-  if (arr.length <= 1) {
-    return [-1];
-  } else {
-    arr.splice(arr.indexOf(Math.min(...arr)), 1);
-    return arr;
-  }
+  function solution(arr) { 
+    // 배열의 요소가 1개인경우 그 값이 최소값이되므로 제거하면 요소는 0개가 된다. 그래서 배열의 길이가 1보다 작거나 같은 경우로 조건을 주고 [-1]을 리턴
+    if(arr.length <= 1){
+        return [-1];
+    } else {
+        arr.splice(arr.indexOf(Math.min(...arr)), 1);
+        return arr;
+    }
 }
 
 console.log(solution([4, 3, 2, 1]));
